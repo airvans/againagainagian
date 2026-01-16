e87ac79c2069854ce8e1e6672d23ce25063eb3a3
quickerserver <quickserv54@gmail.com>
2026-01-16 15:22:07 +0100

    alright i am tired

--- src/!popups.js
+++ src/!popups.js
@@ -1,81 +1,90 @@
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 
-const checkedpopup = document.getElementById("item-achievement")?.querySelector("input");
 
-const popups = localStorage.getItem("popups") 
+const checkedpopup = document
+  .getElementById("item-achievement")
+  ?.querySelector("input");
 
-const popupset = new Set(popups ? JSON.parse(popups) : [])
+const popups = localStorage.getItem("popups");
 
-if(checkedpopup){
-  checkedpopup.addEventListener("click",()=>{
-    if(checkedpopup.checked)checkmark()
-  })
+const popupset = new Set(popups ? JSON.parse(popups) : []);
+
+if (checkedpopup) {
+  checkedpopup.addEventListener("click", () => {
+    if (checkedpopup.checked) checkmark();
+  });
 }
 
+window.dotheshuffle = dotheshuffle;
+window.doacheck = doacheck;
 
 
-function dotheshuffle(){
+function dotheshuffle() {
+  const name = "inspector";
+  const text = "found a probable feature 🎉";
 
-    const name = "inspector"
-    const text = "found a probable feature 🎉"
-
-    if (!popupset.has(name)){
-        showachievement(name, text , "assets/achievements/inspector.png")
-        addachievement(name)
-    }
-
+  if (!popupset.has(name)) {
+    showachievement(name, text, "assets/achievements/inspector.png");
+    addachievement(name);
+  }
 }
 
-function Randomdraw(){
-    const name = "patience or Lucky"
-    const text = "you waited or where lucky to get this"
+function Randomdraw() {
+  const name = "patience or Lucky";
+  const text = "you waited or where lucky to get this";
 
-    if (!popupset.has(name)){
-        showachievement(name, text , "assets/achievements/patience.png")
-        addachievement(name)
-    }
+  if (!popupset.has(name)) {
+    showachievement(name, text, "assets/achievements/patience.png");
+    addachievement(name);
+  }
 }
 
-function checkmark(){
-    const name = "Rule changer"
-    const text = "guess you like to break rules huh"
+function checkmark() {
+  const name = "Rule changer";
+  const text = "guess you like to break rules huh";
 
-    if (!popupset.has(name)){
-        showachievement(name, text , "assets/achievements/rulechanger.png")
-        addachievement(name)
-    }
+  if (!popupset.has(name)) {
+    showachievement(name, text, "assets/achievements/rulechanger.png");
+    addachievement(name);
+  }
 }
 
-function doacheck(){
-    const input = document.querySelector("#name-checker input").value.toLowerCase().trim().replaceAll(" ","")
-    const name = "Guess your right"
-    const text = "You know your stuff huh?"
+function doacheck() {
+  const input = document
+    .querySelector("#name-checker input")
+    .value.toLowerCase()
+    .trim()
+    .replaceAll(" ", "");
+  const name = "Guess your right";
+  const text = "You know your stuff huh?";
 
-    if (input == "champagnecoast" && !popupset.has(name)){
-        showachievement(name, text , "assets/achievements/namechecker.png")
-        addachievement(name)
-    }
+  if (input == "champagnecoast" && !popupset.has(name)) {
+    showachievement(name, text, "assets/achievements/namechecker.png");
+    addachievement(name);
+  }
 }
 
+showachievement(null, null, null)
 
-
-function showachievement(name, text , img) {
-
+function showachievement(name, text, img) {
+  Toastify({
+    text: `This is a toast ${name}`,
+    duration: 3000,
+  }).showToast();
 }
 
-
-function addachievement(name){
-    popupset.add(name)
-    localStorage.setItem("popups", JSON.stringify([...popupset]))
-    overachiever()
+function addachievement(name) {
+  popupset.add(name);
+  localStorage.setItem("popups", JSON.stringify([...popupset]));
+  overachiever();
 }
 
-
-function overachiever(){
-    if(popupset.size == 4){
-        const finalname = "overachiever"
-        const finaltext = "The end doesnt detere you" 
-        showachievement(finalname, finaltext, "assets/achievements/master.png")
-        addachievement(finalname)
-    }
+function overachiever() {
+  if (popupset.size == 4) {
+    const finalname = "overachiever";
+    const finaltext = "The end doesnt detere you";
+    showachievement(finalname, finaltext, "assets/achievements/master.png");
+    addachievement(finalname);
+  }
 }
-
